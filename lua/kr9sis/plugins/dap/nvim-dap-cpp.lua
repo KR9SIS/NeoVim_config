0959e37c95e5997cfa8be20d67548c969af93b2b
kr9sis <kristoferhelgi@protonmail.com>
2024-11-10 14:40:46 +0000

    Moved all dap files to seperate folder and added c++ configuration

---
+++ lua/kr9sis/plugins/dap/nvim-dap-cpp.lua
@@ -0,0 +1,42 @@
+return {
+  "mfussenegger/nvim-dap",
+  ft = { "cpp", "c" },
+  event = { "BufReadPre", "BufNewFile", "VeryLazy" },
+  config = function()
+    local dap = require("dap")
+
+    dap.adapters.codelldb = {
+      type = "server",
+      port = "${port}",
+      executable = {
+        command = "codelldb",
+        args = { "--port", "${port}" },
+      },
+    }
+
+    dap.configurations.cpp = {
+      {
+        name = "Launch file",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+        args = {},
+        runInTerminal = false,
+      },
+      {
+        name = "Attach to process",
+        type = "codelldb",
+        request = "attach",
+        pid = require("dap.utils").pick_process,
+        args = {},
+      },
+    }
+
+    -- Also support C files
+    dap.configurations.c = dap.configurations.cpp
+  end,
+}
