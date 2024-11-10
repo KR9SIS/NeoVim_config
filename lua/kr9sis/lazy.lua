fd87594e264b81b4a367df1f94cb0b8b89c0c57e
kr9sis <kristoferhelgi@protonmail.com>
2024-11-10 14:40:46 +0000

    Moved all dap files to seperate folder and added c++ configuration

--- lua/kr9sis/lazy.lua
+++ lua/kr9sis/lazy.lua
@@ -11,12 +11,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "kr9sis.plugins" }, { import = "kr9sis.plugins.lsp" } }, {
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
-})
+require("lazy").setup(
+  { { import = "kr9sis.plugins" }, { import = "kr9sis.plugins.lsp" }, { import = "kr9sis.plugins.dap" } },
+  {
+    checker = {
+      enabled = true,
+      notify = false,
+    },
+    change_detection = {
+      notify = false,
+    },
+  }
+)
